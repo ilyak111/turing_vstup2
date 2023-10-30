bc698dec0e8f235cfc69ac1f9925884d33d1d7ea
Ilya <thediamondplay.777@gmail.com>
2023-10-31 01:23:26 +0300

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    val str = readln()
+    val words = if (str.isNotEmpty()) str.split(" ") else listOf()
+    val correctWords: MutableList<String> = mutableListOf()
+    for (word in words)
+        if (word[0] == 'а' && word.length > 3)
+            correctWords.add(word)
+    if (correctWords.isEmpty())
+        println("Слов на 'а' больше 3 символов не найдено")
+    else {
+        print("Слова на 'а' больше 3 символов: ")
+        correctWords.forEach{print("$it ")}
+        println()
+    }
+}
