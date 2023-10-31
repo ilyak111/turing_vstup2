e8f0dda35d27c753c2f6f71d9be57fa00285012d
ilyak111 <thediamondplay.777@gmail.com>
2023-10-31 17:54:48 +0300

    added comments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,15 @@
 fun main() {
-    val str = readln()
-    val words = if (str.isNotEmpty()) str.split(" ") else listOf()
-    val correctWords: MutableList<String> = mutableListOf()
-    for (word in words)
-        if (word[0] == 'а' && word.length > 3)
-            correctWords.add(word)
-    if (correctWords.isEmpty())
-        println("Слов на 'а' больше 3 символов не найдено")
+    val str = readln() //Считываем строку
+    val words = if (str.isNotEmpty()) str.split(" ") else listOf() //Разбиваем строку на слова разделённые пробелами и заносим их в список
+    val correctWords: MutableList<String> = mutableListOf() //Создаём пустой список удовлетворяющих условиям слов
+    for (word in words) //Проходимся по всем словам из списка слов
+        if (word[0] == 'а' && word.length > 3) //Если удовлетворяет условиям
+            correctWords.add(word) //Добавить слово в список удовлетворяющих условиям слов
+    if (correctWords.isEmpty()) //Если список удовлетворяющих условиям слов пуст
+        println("Слов на 'а' больше 3 символов не найдено") //То выводим сообщение об этом
     else {
-        print("Слова на 'а' больше 3 символов: ")
-        correctWords.forEach{print("$it ")}
+        print("Слова на 'а' больше 3 символов: ") //Если нет
+        correctWords.forEach{print("$it ")} //То выводим все эти слова, удовлетворяющие условиям
         println()
     }
 }
