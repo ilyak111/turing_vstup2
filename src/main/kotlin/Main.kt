f8d149aba8ad21aca0dbb7b9fee057d793143f33
ilyak111 <thediamondplay.777@gmail.com>
2023-11-04 14:11:13 +0300

    a bug has been fixed, which, when the user enters two or more spaces (in this case, an empty string is obtained when cropping by spaces), leads to the program crashing due to checking the first character of an empty string in the for loop

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ fun main() {
     val words = if (str.isNotEmpty()) str.split(" ") else listOf() //Разбиваем строку на слова разделённые пробелами и заносим их в список
     val correctWords: MutableList<String> = mutableListOf() //Создаём пустой список удовлетворяющих условиям слов
     for (word in words) //Проходимся по всем словам из списка слов
-        if (word[0] == 'а' && word.length > 3) //Если удовлетворяет условиям
+        if (word.length > 3 && word[0] == 'а') //Если удовлетворяет условиям
             correctWords.add(word) //Добавить слово в список удовлетворяющих условиям слов
     if (correctWords.isEmpty()) //Если список удовлетворяющих условиям слов пуст
         println("Слов на 'а' больше 3 символов не найдено") //То выводим сообщение об этом
